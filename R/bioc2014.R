11e21693b6f850f6f9a69de3c99be4cda3023da0
Laurent <lg390@cam.ac.uk>
2014-07-28 01:34:30 +0100

    use render

---
+++ R/bioc2014.R
@@ -0,0 +1,8 @@
+##' Opens the package vignette.
+##'
+##' @title Bioc2014 vignette
+##' @return Used for its side effects.
+##' @author Laurent Gatto
+bioc2014 <- function()
+    vignette("Bioc2014", package = "ProteomicsBioc2014Workshop")
+
